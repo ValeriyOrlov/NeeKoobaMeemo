46f8cfad63533f7284838373c2aaeff77cf96158
Valerrriy Orlov <wrestlbird@gmail.com>
2026-09-14 21:43:54 +0300

    feat:добавлен периодический пинг на сервер во избежание разрыва соединения через 60 секунд

--- game/internal/game/client.go
+++ game/internal/game/client.go
@@ -3,21 +3,44 @@ package game
 import (
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/ValeriyOrlov/NeeKoobaMeemo/game/internal/models"
 	"github.com/gorilla/websocket"
 )
 
+const (
+	writeWait  = 10 * time.Second
+	pongWait   = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10 // Отправлять пинг каждые ~54 секунды
+)
+
 // WritePump пересылает сообщения из Go-канала client.Send в сеть (WebSocket)
 func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
 	defer func() {
+		ticker.Stop()
 		c.Conn.Close()
 	}()
 
-	for message := range c.Send {
-		err := c.Conn.WriteMessage(websocket.TextMessage, message)
-		if err != nil {
-			break
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.Conn.WriteMessage(websocket.TextMessage, message); err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			// Автоматическая отправка Ping-кадра клиенту
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
 		}
 	}
 }
@@ -28,6 +51,13 @@ func (c *Client) ReadPump(room *Room) {
 		room.Leave(c)
 		c.Conn.Close()
 	}()
+	c.Conn.SetReadLimit(1024)
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	// Сбрасываем таймер таймаута при каждом получении Pong от браузера
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
 
 	for {
 		_, p, err := c.Conn.ReadMessage()
